8872dd0030d7a6d7bd37f2d01d19d1695e6e623a
hashir500 <202951529+hashir500@users.noreply.github.com>
2026-04-02 14:07:07 +0500

    Sleep and feed function

--- pet.py
+++ pet.py
@@ -47,13 +47,39 @@ class Pet:
                 self.health = max(0, self.health)
                 self.hunger = max(0, self.hunger)
                 self.happiness = max(0, self.happiness)
-        
 
+    def feed(self):
+            
+            if self.hunger == self.hungerMax:
+                print(f"I am full")
+                return
+    
+            print(f"Feeding {self.name}")
+            self.hunger += 10
+            self.happiness +=2
+            
+
+            if self.hunger >= self.hungerMax:
+                self.hunger = self.hungerMax
+
+            if self.happiness >= self.happinessMax:
+                self.happiness = self.happinessMax 
+
+            print(f"Thank you for the meal!")
+            
+
+
+    def sleep(self):
+            print("Going to sleep, Bye!")
+            self.energy = min(self.energyMax,self.energy + 10)
+            self.hunger = max(0,self.hunger - 5)
+            self.happiness = min(self.happinessMax,self.happiness + 5)
+            print("I am back now, missed me?")
+         
         
         
 def main():
    p1 = Pet(name='Buddy')
-    # To test, check the status after the logic runs
    p1.time_passes() 
    print(p1.get_status())
 
